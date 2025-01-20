820349a5b88431100eed3367c1489b23928ce181
hammadrauf <rauf.hammad@gmail.com>
2025-01-19 21:54:09 -0500

    Added frame animation. ScreenOff_function does not work properly in actual Windows Screen-Saver .scr.

--- url-monitor-saver.py
+++ url-monitor-saver.py
@@ -25,6 +25,10 @@ elif __file__:
 
 config_file_path = os.path.join(application_path, config_file_name)
 
+# tick in milli seconds, multiple of 30. Used in making Frame animation smooth.
+tick=30
+rtick=tick/1000
+
 '''
 Author: Hammad Rauf (rauf.hammad@gmail.com)
 License: MIT (Open Source, Free)
@@ -47,6 +51,9 @@ Sample YAML file contents:
   frame_height: 1000
   screenoff_enabled: False                                                     
 '''
+#Frame starting position
+winx = 0
+winy = 0
 
 config={}
 try:   
@@ -70,6 +77,8 @@ seconds_on = config['seconds_on']
 frame_width = config['frame_width']
 frame_height = config['frame_height']
 screenoff_enabled = config['screenoff_enabled']
+speed_x = config['speed_x']
+speed_y = config['speed_y']
 
 def generate_auth_hash(use_remote_addr, uname=username, pwd_hash=password_hash, remote_address=domain, zm_auth_secret=secret_key):
     current_time = time.localtime()
@@ -113,10 +122,20 @@ authenticated_url = base_url+generate_auth_hash(False)
 terminate_event = threading.Event()
 
 def destroy(window, timeInSec):
-    for _ in range(timeInSec):
+    global speed_x, speed_y, winx, winy, rtick, tick
+    for _ in range(timeInSec*tick):
         if terminate_event.is_set():
             break
-        time.sleep(1)
+        time.sleep(rtick)
+        if winx + frame_width > webview.screens[0].width or winx < 0:
+            speed_x = speed_x * -1
+        if winy + frame_height > webview.screens[0].height or winy < 0:
+            speed_y = speed_y * -1
+        nx = winx + speed_x * rtick
+        ny = winy + speed_y * rtick
+        winx = int(nx)
+        winy = int(ny)
+        window.move(winx, winy)  
     window.destroy()
 
 def on_key_event(key):
@@ -135,37 +154,45 @@ def mouse_listener():
     with mouse.Listener(on_move=on_mouse_event) as listener:
         listener.join()
 
-# Create and start the keypress and mouse listener threads
-keyboard_listenerO = keyboard.Listener(on_press=on_key_event)
-mouse_listenerO = mouse.Listener(on_move=on_mouse_event)
-keyboard_thread = threading.Thread(target=keyboard_listenerO.start)
-mouse_thread = threading.Thread(target=mouse_listenerO.start)
-keyboard_thread.start()
-mouse_thread.start()
 
-# Main program code
-while not terminate_event.is_set():
-    screenOn()
-    mywin = webview.create_window('ZoneMinder', authenticated_url, width=frame_width, height=frame_height)
-    webview.start(destroy, (mywin, seconds_on))
-    if terminate_event.is_set():
+try:
+    # Create and start the keypress and mouse listener threads
+    keyboard_listenerO = keyboard.Listener(on_press=on_key_event)
+    mouse_listenerO = mouse.Listener(on_move=on_mouse_event)
+    keyboard_thread = threading.Thread(target=keyboard_listenerO.start)
+    mouse_thread = threading.Thread(target=mouse_listenerO.start)
+    keyboard_thread.start()
+    mouse_thread.start()
+
+    # Main program code
+    while not terminate_event.is_set():
         screenOn()
-        break
-    
-    screenOff()
-    for _ in range(seconds_off):
+        mywin = webview.create_window('ZoneMinder', authenticated_url, x=winx, y=winy, 
+                                    width=frame_width, height=frame_height, draggable=True)
+        webview.start(destroy, (mywin, seconds_on))
         if terminate_event.is_set():
             screenOn()
             break
-        time.sleep(1)    
+        
+        screenOff()
+        for _ in range(seconds_off*tick):
+            if terminate_event.is_set():
+                screenOn()
+                break
+            time.sleep(rtick)    
 
-# Stop the listeners if the program is terminating
-keyboard_listenerO.stop()
-mouse_listenerO.stop()
+    # Stop the listeners if the program is terminating
+    keyboard_listenerO.stop()
+    mouse_listenerO.stop()
 
-# Wait for the threads to finish
-keyboard_thread.join()
-mouse_thread.join()
-# if adding threads, then join them here.
+    # Wait for the threads to finish
+    keyboard_thread.join()
+    mouse_thread.join()
+    # if adding threads, then join them here.
 
-# end.
+    # end.
+except Exception as e:
+    print(f"Error: {e}")
+    sys.exit(1)
+else:
+    sys.exit(0)
